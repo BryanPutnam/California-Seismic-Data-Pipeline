7a0e41eb769ea26f104654dfe14e22b74f2cc353
Bryan Putnam <bryanputnam25@gmail.com>
2025-11-11 15:22:12 -0800

    Refactor ProducerApp for better code clarity/cleanliness

--- src/main/java/com/sdp/producer/ProducerApp.java
+++ src/main/java/com/sdp/producer/ProducerApp.java
@@ -6,40 +6,26 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.RecordMetadata;
 
 import java.util.Properties;
-
+import java.io.IOException;
 import java.net.URI; 
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
 
 public class ProducerApp { 
 
-    // URI for USGS Earthquake Data (All Earthquakes in the Past Hour)
     static final String USGS_EARTHQUAKE_API_URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson";
+    static final String KAFKA_TOPIC = "earthquake_data"; 
+    static final String TOPIC_KEY = "quake-1"; 
 
     public static void main(String[] args) { 
-
-        // Kafka Producer Config
-        Properties props = new Properties(); 
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); 
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer"); 
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer"); 
-
-        // Create Kafka Producer
-        KafkaProducer<String, String> producer = new KafkaProducer<>(props); 
+        KafkaProducer<String, String> producer = new KafkaProducer<>(getProducerConfig()); 
 
         try { 
-            HttpClient client = HttpClient.newHttpClient(); 
-            HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(USGS_EARTHQUAKE_API_URL))
-                .build();
-
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            String jsonResponse = response.body();
-
-            ProducerRecord<String, String> record = new ProducerRecord<String, String>("earthquake_data", "quake-1", jsonResponse); // (topic, key, value)
-
-            //Sychronous send (waits for acknowledgement)
+            ProducerRecord<String, String> record = new ProducerRecord<String, String>(KAFKA_TOPIC, TOPIC_KEY, getData(USGS_EARTHQUAKE_API_URL)); 
+            
+            // Synchronous send: waits for Kafka to confirm message delivery and records metadata after
             RecordMetadata metadata = producer.send(record).get(); 
 
             System.out.printf("Sent record(key=%s value=%s) meta(partition=%d, offset=%d)%n",
@@ -52,8 +38,23 @@ public class ProducerApp {
         }
     }
 
-    // For JUnit Test
-    public static URI getURI() { 
-        return URI.create(USGS_EARTHQUAKE_API_URL);
+    private static String getData(String url) throws IOException, InterruptedException{ 
+        HttpClient client = HttpClient.newHttpClient(); 
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(URI.create(url))
+            .timeout(Duration.ofSeconds(10))
+            .build(); 
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String jsonResponse = response.body(); 
+        return jsonResponse; 
     }
+
+    private static Properties getProducerConfig() { 
+        Properties props = new Properties(); 
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); 
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer"); 
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer"); 
+        return props; 
+    }
+
 }
